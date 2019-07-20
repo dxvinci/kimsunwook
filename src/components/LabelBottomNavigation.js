c0810a8e14fa4acb5837004d3613165b2294bc56
sunwook kim <koownusmik@gmail.com>
2019-07-20 12:23:46 +0900

    feat: 뱃지 하드코딩 , handleChange 이벤트 막기

--- src/components/LabelBottomNavigation.js
+++ src/components/LabelBottomNavigation.js
@@ -1,66 +1,106 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
-import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import FaceIcon from '@material-ui/icons/Face';
-import { Link } from 'react-router-dom';
-import { Icon } from 'react-icons-kit'
-import {shop} from 'react-icons-kit/entypo/shop'
+import LocalDrinkIcon from "@material-ui/icons/LocalDrink";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { Link } from "react-router-dom";
+import { Icon } from "react-icons-kit";
+import { shop } from "react-icons-kit/entypo/shop";
 
-
-
-
+import { makeStyles } from "@material-ui/core/styles";
+import Badge from "@material-ui/core/Badge";
 
 const styles = theme => ({
-  root:{
-    height:"10vh",
-    position:"fixed",
-    bottom:0,
-    zIndex:100,
-    boxSizing:"border-box",
-    width:"100%"
-
+  root: {
+    height: "10vh",
+    position: "fixed",
+    bottom: 0,
+    zIndex: 100,
+    boxSizing: "border-box",
+    width: "100%",
+    // margin: theme.spacing(2)
   },
   button: {
     color: "#1E1E1E",
-    boxSizing:"border-box"
+    boxSizing: "border-box"
   },
-  hoveredStyle:{
-    cursor: 'initial'
+  hoveredStyle: {
+    cursor: "initial"
+  },
+  badge: {
+    // padding: theme.spacing(0, 2),
+    color: "#1E1E1E",
+    boxSizing: "border-box"
   }
 });
 
 class LabelBottomNavigation extends React.Component {
-  state = {
-    value: 'menu',
-  };
+  // state = {
+  //   value: "menu"
+  // };
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
+  // handleChange = (event, value) => {
+  //   this.setState({ value });
+  // };
 
   render() {
     const { classes } = this.props;
-    const { value } = this.state;
+    // const { value } = this.state;
+    
 
     return (
-      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root} >
-        <BottomNavigationAction component={Link} to="/menu"label="Menu" value="Menu"  icon={<LocalDrinkIcon />}  className={classes.button}/>
-        <BottomNavigationAction component={Link} to="/cart"label="Cart" value="Cart" icon={<ShoppingCartIcon />} className={classes.button}/>
-        <BottomNavigationAction component={Link} to="/pickup"label="Pickup" value="Pickup" icon={<ReceiptIcon />} className={classes.button}/>
-        <BottomNavigationAction component={Link} to="/cafes"label="Cafes" value="otherCafe" icon={<Icon icon={shop} size={20}/>} className={classes.button} />
+      <BottomNavigation
+        // value={value}
+        // onChange={this.handleChange}
+        className={classes.root}
+      >
+        <BottomNavigationAction
+          component={Link}
+          to="/menu"
+          label="Menu"
+          value="Menu"
+          icon={<LocalDrinkIcon />}
+          className={classes.button}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/cart"
+          label="Cart"
+          value="Cart"
+          icon={
+            <Badge className={classes.badge} badgeContent={1} color="secondary">
+              <ShoppingCartIcon />
+            </Badge>
+          }
+          // className={classes.button}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/pickup"
+          label="Pickup"
+          value="Pickup"
+          icon={<ReceiptIcon />}
+          className={classes.button}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/cafes"
+          label="Cafes"
+          value="otherCafe"
+          icon={<Icon icon={shop} size={20} />}
+          className={classes.button}
+        />
       </BottomNavigation>
     );
   }
 }
 
 LabelBottomNavigation.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(LabelBottomNavigation);
