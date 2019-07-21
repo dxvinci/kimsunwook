9a13de9abc6fbcd77d5d6e1cc50b0a2ec939326a
sunwook kim <koownusmik@gmail.com>
2019-07-21 14:24:52 +0900

    주문없을시 픽업화면 변경
    1. isOrderd 가 있을때만 픽업화면 보이도록 함.
    
    2. 서버로그인이 일단 변경

--- src/routes/Pickup.js
+++ src/routes/Pickup.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import LabelBottomNavigation from '../components/LabelBottomNavigation'
 import SimpleCard from '../components/SimpleCard'
 import {Redirect} from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 
 class Pickup extends Component {
+    state ={
+        isOrdered : false
+    }
     render() {
     const {user , orders, total} = this.props
+    const {isOrdered} = this.state;
         const pickup = {
             paddingTop: 16,
             height: "90vh",
@@ -13,12 +18,26 @@ class Pickup extends Component {
             alignItems: 'center',
         }
         return user ? (
-            <div>
+            isOrdered ? (
+                <div>
                 <div style={pickup}>
                     <SimpleCard orders={orders}/>
                 </div>
                 <LabelBottomNavigation orders={orders}/>
             </div>
+            ) : (
+                <div style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "90vh"
+                }}>
+                    <Typography>주문이 없습니다!</Typography>
+                    <LabelBottomNavigation orders={orders}/>
+
+                </div>
+            )
+  
         ) : (
             <Redirect to="/" />
         )
